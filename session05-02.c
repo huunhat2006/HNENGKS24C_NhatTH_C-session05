8b2635986e9fa95c9d002df746dda8803533d55f
Huunhat123 <huunhat20052006@gmail.com>
2024-11-25 20:05:36 +0700

    session05-02

---
+++ session05-02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i =31;
+	int num;
+	do{ 
+    printf("Moi ban nhap gia tri cho bien num:");
+	scanf("%d",&num);
+    }
+	while(num != i);
+	return 1;
+
+ }
+
